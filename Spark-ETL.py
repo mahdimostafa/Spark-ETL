575d8544ceb62e5d978819c3a7c301a6c00433b9
mahdimostafa <mahdi.mostafa.2012@gmail.com>
2019-06-07 16:13:23 +0100

    Adding pep8 and code cleaning

--- Spark-ETL.py
+++ Spark-ETL.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  5 13:42:11 2019
-
-@author: mahdimostafa
-"""
 
 import configparser
 import datetime
@@ -18,13 +13,24 @@ from pyspark.sql.functions import monotonically_increasing_id
 
 
 config = configparser.ConfigParser()
-config.read_file(open(os.path.abspath('Desktop/git/spark-etl/dl.cfg')))
+config.read_file(open(os.path.abspath('dl.cfg')))
 
 os.environ["AWS_ACCESS_KEY_ID"]= config['AWS']['ACCESS_KEY']
 os.environ["AWS_SECRET_ACCESS_KEY"]= config['AWS']['SECRET_KEY']
 
 
 def create_spark_session():
+
+"""
+    Description: This function can be used to set up the spark connection string and set up
+    the spark environment.
+
+    Arguments:
+    spark: the spark object.  
+
+    Returns:
+        spark
+"""
     spark = SS \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
@@ -33,11 +39,11 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
-    
+
     song_data = input_data+"song_data/*/*/*/*.json"
-    
+
     df = spark.read.json(song_data)
-    
+
     songs_table = df['song_id', 'title', 'artist_id', 'year', 'duration']
     songs_table.printSchema()
     songs_table.show()
@@ -45,11 +51,11 @@ def process_song_data(spark, input_data, output_data):
     songs_table.write.partitionBy('year', 'artist_id').parquet(os.path.join(output_data, 'songs.par'), 'overwrite')
 
     artists_table = df['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']
-   
+
     artists_table = artists_table.dropDuplicates(['artist_id'])
 
     artists_table.write.parquet(os.path.join(output_data, 'artists.par'), 'overwrite')
-    
+
 def process_log_data(spark, input_data, output_data):
 
     log_data = spark.read.json('Desktop/git/spark-etl/2018-11-01-events.json')
@@ -57,7 +63,7 @@ def process_log_data(spark, input_data, output_data):
     df = log_data
 
     df = df[df['page'] == 'NextSong']
-  
+
     users_table = df['userId','firstName','lastName','gender','level']
 
     users_table = users_table.dropDuplicates(['userId'])
@@ -79,7 +85,7 @@ def process_log_data(spark, input_data, output_data):
     get_weekday = udf(lambda x : datetime.datetime.fromtimestamp(x/1000.0).strftime('%a'))
 
     df = df.withColumn('month',get_month(df.ts)).withColumn('day',get_day(df.ts)).withColumn('year',
-                      get_year(df.ts)).withColumn('hour', get_hour(df.ts)).withColumn('%w', 
+                      get_year(df.ts)).withColumn('hour', get_hour(df.ts)).withColumn('%w',
                               get_week(df.ts)).withColumn('%a', get_weekday(df.ts))
     filtercols = ['month', 'day', 'year', 'hour','%w', '%a']
     time_table =  df.select(filtercols)
@@ -87,25 +93,25 @@ def process_log_data(spark, input_data, output_data):
     time_table.write.partitionBy('year', 'month').parquet(os.path.join(output_data, 'time.par'), 'overwrite')
 
 
-    song_df = spark.read.parquet('s3://output_data/songs.par') 
- 
+    song_df = spark.read.parquet('s3://output_data/songs.par')
+
 
     df_join = log_data.join(song_data, log_data.artist == song_data.artist_name)
     df = df_join
     df = df.withColumn("songplay_id", monotonically_increasing_id())
 
-    
+
     songplays_table = df['songplay_id','ts','userId', 'level', 'song_id', 'artist_id', 'sessionId', 'location', 'userAgent']
-    
+
     # write songplays table to parquet files partitioned by year and month
     songplays_table.write.parquet(os.path.join(output_data,"songplay"), 'overwrite')
-    
+
 def main():
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
     output_data = "s3a://output_data/"
-    
-    process_song_data(spark, input_data, output_data)    
+
+    process_song_data(spark, input_data, output_data)
     process_log_data(spark, input_data, output_data)
 
 
