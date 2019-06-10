621018a46a92a6ad19e2c3a1da43e637c56686ef
mahdimostafa <mahdi.mostafa.2012@gmail.com>
2019-06-10 20:21:18 +0100

    process data

--- Spark-ETL.py
+++ Spark-ETL.py
@@ -103,6 +103,9 @@ def main():
     input_data = "s3a://udacity-dend/"
     output_data = "s3a://output_data/"
 
+    process_song_data(spark, input_data, output_data)
+    process_log_data(spark, input_data, output_data)
+
 
 if __name__ == "__main__":
     main()
